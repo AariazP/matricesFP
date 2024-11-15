46144ff26ebbb30dfdb23086f8d29bcd963c6512
Anubis <correaanubis@gmail.com>
2024-11-15 10:29:21 -0500

    (UPDATE) se corrigen los graficos de los algoritmos de Anubis

--- scripts/tomarTiemposAlgoritmos.sh
+++ scripts/tomarTiemposAlgoritmos.sh
@@ -22,7 +22,9 @@ mkdir -p "../Resultados/${nombre_algoritmo}/$entorno/tiempo"
 archivo_salida_resultado="../Resultados/${nombre_algoritmo}/$entorno/resultadomultiplicacion/resultado_${nombre_algoritmo}_${tamanio_matriz}.txt"
 archivo_salida_tiempo="../Resultados/${nombre_algoritmo}/$entorno/tiempo/tiempo_${nombre_algoritmo}_${tamanio_matriz}.txt"
 # Medir el tiempo de ejecución del comando
-tiempo=$( { time eval "$comando" > "$archivo_salida_resultado"; } 2>&1 | grep real | awk '{print $2}')
+tiempo=$( { time eval "$comando" > "$archivo_salida_resultado"; } 2>&1 | grep real | awk '{print $2}' | grep -oP "(?<=m).+(?=s)" )
+
+tiempo=${tiempo/,/.}
 
 
 echo "Tiempo de ejecución: $tiempo segundos" > "$archivo_salida_tiempo"
